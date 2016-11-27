c51353b9a1b9e908313af08f7c1051575eda3618
miramardesign <miramardesign@gmail.com>
2016-11-27 15:53:13 -0300

    first commit

---
+++ WindowsFormsApplication1/DisplayForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ShutdownServerWithHdmi
+{
+    partial class DisplayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.display1 = new System.Windows.Forms.Label();
+            this.display2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // display1
+            // 
+            this.display1.AutoSize = true;
+            this.display1.Location = new System.Drawing.Point(37, 96);
+            this.display1.Name = "display1";
+            this.display1.Size = new System.Drawing.Size(59, 16);
+            this.display1.TabIndex = 0;
+            this.display1.Text = "display1";
+            // 
+            // display2
+            // 
+            this.display2.AutoSize = true;
+            this.display2.Location = new System.Drawing.Point(37, 140);
+            this.display2.Name = "display2";
+            this.display2.Size = new System.Drawing.Size(0, 16);
+            this.display2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 244);
+            this.Controls.Add(this.display2);
+            this.Controls.Add(this.display1);
+            this.Name = "Form1";
+            this.Text = "ShowDisplays";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label display1;
+        private System.Windows.Forms.Label display2;
+    }
+}
+
---
+++ WindowsFormsApplication1/DisplayForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ShutdownServerWithHdmi
+{
+    public partial class DisplayForm : Form
+    {
+
+        // https://msdn.microsoft.com/en-us/library/system.windows.forms.timer.tick.aspx
+        public DisplayForm()
+        {
+            
+
+            InitializeComponent();
+
+            RunTimer();
+        }
+
+     
+
+        public void RunTimer()
+        {
+
+            /* Adds the event and the event handler for the method that will 
+             process the timer event to the timer. */
+            myTimer.Tick += new EventHandler(TimerEventProcessor);
+
+            // Sets the timer interval to 5 seconds.
+            myTimer.Interval = 3000;
+            myTimer.Start();
+
+
+            // Processes all the events in the queue.
+            Application.DoEvents();
+
+        }
+
+
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        static int alarmCounter = 10;
+
+        // This is the method to run when the timer is raised.
+        private  void TimerEventProcessor(Object myObject,
+                                                EventArgs myEventArgs)
+        {
+
+            // Restarts the timer and increments the counter.
+            alarmCounter += 1;
+            myTimer.Enabled = true;
+
+
+            ShowScreens();
+
+            
+
+        }
+
+        public void ClearText()
+        {
+            display1.Text = "";
+            display2.Text = "";
+        }
+
+
+        public  void ShowScreens()
+        {
+            ClearText();
+
+           
+            Screen[] monitors = Screen.AllScreens;
+
+            string output = "";
+            foreach (Screen monitor in monitors)
+            {
+                output+= "Bits Per Pixel: " + monitor.BitsPerPixel;
+                output+= "Bounds: " + monitor.Bounds;
+                output+= "Device Name: " + monitor.DeviceName;
+                output+= "Is Primary: " + monitor.Primary;
+                output+= "Working Area: " + monitor.WorkingArea + "/n";
+            }
+
+            display1.Text = output;
+
+      
+
+        }
+    }
+}
